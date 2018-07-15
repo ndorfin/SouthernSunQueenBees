83fed0cf1940cff24d1742228f46325250a59cbf
Shaun O'Connell <ndorfin@gmail.com>
2018-07-15 20:27:59 +1200

    Get the homepage to generate everything.

--- helpers/contentful_helpers.rb
+++ helpers/contentful_helpers.rb
@@ -16,7 +16,6 @@ module ContentfulHelpers
     top_level_pages = get_pages().select do |page_id, page|
       page.show_in_top_level_menu == true
     end
-    print "top_level_pages: #{top_level_pages}\n"
     top_level_pages.to_a
   end
 end
