1239d7f66c59fbffd9dec590894702eeb739d176
ndorfin <ndorfin@users.noreply.github.com>
2024-10-25 11:00:01 +0000

    deploy: 36cc4aa1a557952e438a39e830ab1643d61dac29

---
+++ mjs/preview_product_page-633e6327.js
@@ -0,0 +1,52 @@
+import PreviewForm from '/mjs/wc/preview-form-996b1261.js';
+
+const mdConverter = new window.showdown.Converter();
+
+function buildProductDescription(description) {
+  document.querySelector('.product_description').innerHTML = mdConverter.makeHtml(description);
+}
+
+function buildExtraContent(fields) {
+  const productName = fields.title;
+  const arrayTemplateFields = [
+    {
+      keyName: 'aboutThisProduct',
+      title: `About our ${ productName }`
+    },
+    {
+      keyName: 'orderInformation',
+      title: `Ordering ${ productName }`
+    },
+    {
+      keyName: 'paymentAndDeliveryDetails',
+      title: `Payment and delivery information`
+    },
+  ];
+  let htmlStringExtraContent = '';
+
+  arrayTemplateFields.forEach(({keyName, title}) => {
+    if (fields[keyName]) {
+      htmlStringExtraContent += `
+        <details open class="product_info_section">
+          <summary class="heading heading_subheading">${ title }</summary>
+          ${ documentToHtmlString(fields[keyName]) }
+        </details>
+      `;
+    }
+  });
+
+  document.querySelector('.product_info').insertAdjacentHTML('beforeend', htmlStringExtraContent);
+}
+
+function parameterize(string) {
+  return string.replace(/[^a-zA-Z\d]/g, '-');
+}
+
+function buildPage(fields) {
+  buildProductDescription(fields.description)
+  buildExtraContent(fields);
+  document.body.classList.add(`page_product_${parameterize(fields.slug)}`);
+}
+
+window.buildPage = buildPage;
+window.customElements.define('preview-form', PreviewForm);
